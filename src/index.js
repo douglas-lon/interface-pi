b876d8088e48e7476766cdf81c4ee43386704b9b
Douglas Sousa <playerdoug60@gmail.com>
2023-05-21 21:13:44 -0300

    e

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { NavBar } from './components/NavBar';
-import Dashboard  from './pages/visualizacao/Dashboard';
 import DashboardRendimento from './pages/visualizacao/DashboardRendimento';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
